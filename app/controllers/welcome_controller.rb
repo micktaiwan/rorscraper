9e30386933ca1e11f91a6bb9ebbc61fc4756c22f
faivrem <faivrem@8f529122-4252-11de-9cfd-b5a970547080>
2009-05-17 23:02:42 +0000

    more scraps

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     redirect_to('/account/login') and return if session['user'] == nil
-    @scraps = session['user'].scraps
+    @scraps = Scrap.find(:all, :conditions=>["user_id=?",session['user'].id])
   end
   
 end
