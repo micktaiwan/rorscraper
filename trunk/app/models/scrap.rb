107ed8f5c416fd5c850209f5fbab438baba6d7e1
faivrem <faivrem@8f529122-4252-11de-9cfd-b5a970547080>
2009-05-17 18:02:01 +0000

    basic scrap runner

---
+++ trunk/app/models/scrap.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+
+class Scrap < ActiveRecord::Base
+
+  def do_scraping!
+    begin
+      open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}",
+        "From" => "protask@protaskm.com",
+        "Referer" => "http://scraper.protaskm.com/") { |f|
+        self.page = f.read
+        self.error= ''
+        save!
+        }
+    rescue Exception => e
+      self.error = "Scraping failed: "+e.message
+      save!
+    end
+  end
+
+end
+
