81af984621c74c4403436ba12d4384ef19f9aa63
faivrem <faivrem@8f529122-4252-11de-9cfd-b5a970547080>
2009-05-17 20:36:00 +0000

    fix

--- trunk/app/controllers/welcome_controller.rb
+++ trunk/app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    redirect_to('/account/login') if session['user'] == nil
+    redirect_to('/account/login') and return if session['user'] == nil
     @scraps = session['user'].scraps
   end
   
