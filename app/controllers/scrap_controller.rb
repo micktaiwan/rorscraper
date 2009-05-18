39b6acb86a9bb6705675e22c1455a40260e0a1ce
faivrem <faivrem@8f529122-4252-11de-9cfd-b5a970547080>
2009-05-18 11:55:53 +0000

    scrap edition

---
+++ app/controllers/scrap_controller.rb
@@ -0,0 +1,31 @@
+class ScrapController < ApplicationController
+
+  def add
+    @scrap = Scrap.new
+    render(:partial=>'se')
+  end
+
+  def edit
+    @scrap = Scrap.find(params['id'])
+    render(:partial=>'se')
+  end
+  
+  def post
+    id = params['id']
+    if id==nil
+      scrap = Scrap.new(params['scrap'])
+      scrap.user_id = session['user'].id
+      scrap.save
+    else
+      Scrap.find(id).update_attributes(params['scrap'])
+    end
+    @scraps = Scrap.find(:all, :conditions=>["user_id=?",session['user'].id])
+    render(:partial=>'scrap', :collection=>@scraps)
+  end
+  
+  def delete
+    Scrap.delete(params['id'])
+    render(:text=>'deleted')
+  end
+
+end
